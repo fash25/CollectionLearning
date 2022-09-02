1f535b626092bb114a78c132a4eeb89446b411ea
fash25 <fash25rus@gmail.com>
2022-09-02 17:04:33 +1000

    Cats example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,9 +1,24 @@
 package ru.synergy;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat( "Tomas" );
+        cats[1] = new Cat( "Hippo" );
+        cats[2] = new Cat( "Franc" );
+        cats[3] = new Cat( "Fruct" );
+
+        cats[1] = null;
+
+        System.out.println( Arrays.toString(cats) );
+
+
 
     }
 }
