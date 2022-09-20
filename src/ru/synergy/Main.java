2a59a31ca4898e744ee54e17a032a4a4618497bc
fash25 <fash25rus@gmail.com>
2022-09-21 09:47:39 +1000

    Added HashSet & TreeSet example & unversioned fille

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -110,6 +110,29 @@ public class Main {
             HashSet<Cat> catHashSet = new HashSet<>(catsList);
             System.out.println(catHashSet);
 
+            // Map
+            Map<Integer, String> statesMap = new HashMap<>();
+
+            statesMap.clear();
+            statesMap.put(1,"Germany");
+            statesMap.put( 2,"Spain" );
+            statesMap.put( 3,"British" );
+            statesMap.put( 4,"Italy" );
+
+            System.out.println(statesMap.keySet());
+
+            statesMap.remove( 2 );
+
+            for (Map.Entry<Integer, String> item : statesMap.entrySet()){
+               System.out.printf("Key:  %d  |  value:  %s \n", item.getKey(), item.getValue());
+
+            }
+
+            String first = statesMap.get( 2 );
+            System.out.println(first);
+
+
+
 
         }
 
